f3ffead1404959602b5dc9d37cf2c544bf3f66e4
Jeff Lawlis <jeff.lawlis@yahoo.com>
2017-01-24 11:17:42 -0600

    setup html main and router

---
+++ app/scripts/router.js
@@ -0,0 +1,8 @@
+import {Router, Route, browserHistory} from 'react-router';
+import React from 'react';
+
+const router = (
+  <Router history={browserHistory}>
+  </Router>
+);
+export default router;
